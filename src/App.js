4cd864770ee0e04772080b6baa63e457039e172a
Kan <zxdfgcv@gmail.com>
2022-08-22 15:06:33 +0800

    Added the final.

---
+++ src/App.js
@@ -0,0 +1,145 @@
+// import './App.css';
+// import {
+//   HashRouter,
+//   NavLink,
+// } from 'react-router-dom';
+
+// const Todo = () => {
+//   return <p>這是 Todo 頁面 
+//   </p>;
+// };
+// const Login = () => {
+//   return <p>這是登入頁面</p>;
+// };
+// const Register = () => {
+//   return <p>這是註冊頁面</p>;
+// };
+
+// function App() {
+//   return (
+//     <div className="container">
+//       <HashRouter>
+//         <div className="nav-link">
+//           <NavLink to="/">
+//             <p>回到首頁</p>
+//           </NavLink>
+//           <NavLink to="/register">
+//             <p>註冊頁面</p>
+//           </NavLink>
+//           <NavLink to="/login">
+//             <p>登入頁面</p>
+//           </NavLink>
+//           <NavLink to="/todo">
+//             <p>Todo 頁面</p>
+//           </NavLink>
+//         </div>
+//         {/* Routes, Route 練習區 */}
+//         {/* 練習區 */}
+//       </HashRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// ==================================================
+// https://github.com/riceball-tw/Day-15-React-Dynamic-Router/blob/main/src/App.js
+
+
+
+import "./App.css";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  NavLink,
+  useNavigate,
+  Outlet,
+  useParams,
+} from "react-router-dom";
+
+const Todo = () => {
+  return (
+    <>
+      <p>Todo</p>
+      <LogOut />
+    </>
+  );
+};
+
+const Login = () => {
+  return <p>這是登入頁面</p>;
+};
+const Register = () => {
+  return <p>這是註冊頁面</p>;
+};
+
+const LogOut = () => {
+  const navigate = useNavigate();
+  return (
+    <button
+      onClick={() => {
+        navigate("/login");
+      }}
+    >
+      登出
+    </button>
+  );
+};
+
+const Post = () => {
+  return (
+    <div>
+      <h3>Post 頁面</h3>
+      <Outlet />
+    </div>
+  );
+};
+
+const PostId = () => {
+  let params = useParams();
+  return <p>Post: {params.postId}</p>;
+};
+
+function App() {
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <div className="nav-link">
+          <NavLink to="/">
+            <p>回到首頁</p>
+          </NavLink>
+          <NavLink to="/register">
+            <p>註冊頁面</p>
+          </NavLink>
+          <NavLink to="/login">
+            <p>登入頁面</p>
+          </NavLink>
+          <NavLink to="/todo">
+            <p>Todo 頁面</p>
+          </NavLink>
+          <NavLink to="/post">
+            <p>Post 頁面</p>
+          </NavLink>
+          <NavLink to="/post/HelloWorld">
+            <p>Post 詳細頁面</p>
+          </NavLink>
+        </div>
+        {/* Routes, Route 練習區 */}
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Todo" element={<Todo />} />
+          <Route path="/post" element={<Post />}>
+            <Route path=":postId" element={<PostId />} />
+          </Route>
+        </Routes>
+        {/* 練習區 */}
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
